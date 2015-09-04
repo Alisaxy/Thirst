39bce6c80fc3cafc72743bbe64220603e0b83cdd
Alisa Dolinsky <alisa.dolinsky.1984@gmail.com>
2015-09-04 22:14:25 +0200

    Encapsulation and proper tail recursion. Every operator has an access to the operator stack, it just needs to put a base of itself (void of operands) onto the operand stack and return its result. That way one can achieve proper tail recursion.

--- src/gluttony.lua
+++ src/gluttony.lua
@@ -5,51 +5,48 @@ local function exit (...) print('result', ...) print('exit') end
 local function operator_prototype (operators, execute)
     local operands = {}
     local rest = {}
-    local stack = operands
     return function (...)
         local args = {...}
         local _
         while (function () _ =
-            table.remove(args, 1)
-            return not (_ == nil or _ == sentinel) end)()
+            table.remove (args, 1)
+            return not (_ == nil or _ == sentinel) end) ()
         do
             table.insert (operands, _)
         end
         if _ == sentinel then
             table.remove (operators)
-            local output = {execute (operands)}
+            local output = { execute (operands, operators) }
             for i, v in ipairs (args) do table.insert (output, v) end
-            operators[#operators] (unpack (output))
+            return output
         end
     end
 end
 
 local function combinator_prototype (op, result)
     return function (operands)
-        for i, v in ipairs(operands) do result = op(result, v) end
+        for i, v in ipairs (operands) do result = op (result, v) end
         return result
     end
+end
+
+function processor_prototype ()
+    local operators = { exit }
+    return { operator = function (op) table.insert (operators, operator_prototype (operators, op)) end,
+             operand = function (...)
+                local _ = operators[#operators] (...)
+                while _ ~= nil do _ = operators[#operators] (unpack (_)) end
+             end }
 end
 
 local add = combinator_prototype (function (x, y) return x + y end, 0)
 local sub = combinator_prototype (function (x, y) return x - y end, 0)
 
-local operators = {exit}
-table.insert (operators, operator_prototype (operators, add))
-operators[#operators] (1, 2)
-operators[#operators] (3, 4)
-operators[#operators] (5, 6)
-table.insert (operators, operator_prototype (operators, sub))
-operators[#operators] (2, 3, sentinel)
-operators[#operators] (sentinel, 7, 8)
-
--- local function test(b)
---     if b then goto left else goto right end
---     ::left:: do
---         return print('left') end
---     ::right:: do
---         return print('right') end
--- end
-
--- test(true)
--- test()
+local app = processor_prototype ()
+app.operator (add)
+app.operand (1, 2)
+app.operand (3, 4)
+app.operand (5, 6)
+app.operator (sub)
+app.operand (2, 3, sentinel)
+app.operand (sentinel, 7, 8)
