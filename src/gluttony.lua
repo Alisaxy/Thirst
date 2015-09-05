f68002a9fbfa8414929ef772b25918c83c078d51
Alisa Dolinsky <alisa.dolinsky.1984@gmail.com>
2015-09-06 00:20:42 +0200

    Eval.

--- src/gluttony.lua
+++ src/gluttony.lua
@@ -1,5 +1,5 @@
 local sentinel = {}
-local null = {}
+local void = {}
 
 local function exit (...) print('result', ...) print('exit') end
 
@@ -10,7 +10,7 @@ local function operator_prototype (operators, execute)
         local _
         while (function () _ =
             table.remove (args, 1)
-            return not (_ == nil or _ == null or _ == sentinel) end) ()
+            return not (_ == nil or _ == void or _ == sentinel) end) ()
         do
             table.insert (operands, _)
         end
@@ -23,17 +23,19 @@ local function operator_prototype (operators, execute)
     end
 end
 
-function processor_prototype ()
-    local operators = { exit }
-    return function (...)
-        for i, v in ipairs ({...}) do
-            if type (v) == 'function' then table.insert (operators, operator_prototype (operators, v)) else
-                local _ = operators[#operators] (v)
-                if operators[#operators] == exit then return operators[#operators] (unpack (_), select (i + 1, ...)) end
-                while _ ~= nil do _ = operators[#operators] (unpack (_)) end
-            end
+function eval (operands, operators)
+    for i, v in ipairs (operands) do
+        if type (v) == 'function' then table.insert (operators, operator_prototype (operators, v)) else
+            local _ = operators[#operators] (v)
+            if operators[#operators] == exit then return operators[#operators] (unpack (_), select (i + 1, unpack (operands))) end
+            while _ ~= nil do _ = operators[#operators] (unpack (_)) end
         end
     end
+end
+
+function processor_prototype ()
+    local operators = { exit }
+    return function (...) eval ({...}, operators) end
 end
 
 local function combinator_prototype (op)
@@ -54,7 +56,7 @@ loop = function (operands, operators)
     local step = operands[3]
     print('loop', unpack(operands))
     if idx < limit then table.insert(operators, operator_prototype (operators, loop)) return idx + step, limit, step, sentinel
-    else return null end
+    else return void end
 end
 
 local app = processor_prototype ()
