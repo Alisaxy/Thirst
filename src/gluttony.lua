930f806136a7fe0112e88e690838d6c4bd53dd35
Alisa Dolinsky <alisa.dolinsky.1984@gmail.com>
2015-09-06 00:08:32 +0200

    Rebase?

--- src/gluttony.lua
+++ src/gluttony.lua
@@ -1,14 +1,16 @@
 local sentinel = {}
+local null = {}
 
-local function exit (args) print('result', unpack (args)) print('exit') end
+local function exit (...) print('result', ...) print('exit') end
 
 local function operator_prototype (operators, execute)
     local operands = {}
-    return function (args)
+    return function (...)
+        local args = {...}
         local _
         while (function () _ =
             table.remove (args, 1)
-            return not (_ == nil or _ == sentinel) end) ()
+            return not (_ == nil or _ == null or _ == sentinel) end) ()
         do
             table.insert (operands, _)
         end
@@ -23,45 +25,45 @@ end
 
 function processor_prototype ()
     local operators = { exit }
-    local handle_operator = function (op) table.insert (operators, operator_prototype (operators, op)) end
-    local handle_operand = function (...)
-                local _ = operators[#operators] ({...})
-                while _ ~= nil do _ = operators[#operators] (unpack (_)) end
-             end
-    local process = function (...)
+    return function (...)
         for i, v in ipairs ({...}) do
-            if type(v) == 'function' then handle_operator (v) else handle_operand (v) end
-            if operators[#operators] == exit then return handle_operator (select (i, ...)) end
+            if type (v) == 'function' then table.insert (operators, operator_prototype (operators, v)) else
+                local _ = operators[#operators] (v)
+                if operators[#operators] == exit then return operators[#operators] (unpack (_), select (i + 1, ...)) end
+                while _ ~= nil do _ = operators[#operators] (unpack (_)) end
+            end
         end
     end
-    return process
 end
 
-local function combinator_prototype (op, result)
+local function combinator_prototype (op)
     return function (operands)
-        for i, v in ipairs (operands) do result = op (result, v) end
-        return { result }
+        local _ = table.remove (operands, 1)
+        for i, v in ipairs (operands) do _ = op (_, v) end
+        return _
     end
 end
 
-local add = combinator_prototype (function (x, y) return x + y end, 0)
-local sub = combinator_prototype (function (x, y) return x - y end, 0)
+local add = combinator_prototype (function (x, y) return x + y end)
+local sub = combinator_prototype (function (x, y) return x - y end)
 
 local loop
 loop = function (operands, operators)
     local idx = operands[1]
     local limit = operands[2]
     local step = operands[3]
-    print ('loop', unpack (operands))
-    if idx < limit then table.insert (operators, operator_prototype (operators, loop)) return {idx + step, limit, step, sentinel}
-    else return { 777 } end
-end
-
-local function quote_prototype ()
-    local quote = function (operands) return { operands } end
-    quote.suspend = true
-    return quote
+    print('loop', unpack(operands))
+    if idx < limit then table.insert(operators, operator_prototype (operators, loop)) return idx + step, limit, step, sentinel
+    else return null end
 end
 
 local app = processor_prototype ()
-app (add, 1, 2, 3, 4, 5, 6, sub, 2, 3, sentinel, loop, 0, 10, 2, sentinel, sentinel, 7, 8)
+app (add, 1, 2, 3, 4, 5, 6, sub, 2, 3, sentinel, loop, 0, 10, 2, sentinel, sentinel, 7, 8, sentinel, sentinel)
+-- app (1, 2)
+-- app (3, 4)
+-- app (5, 6)
+-- app (sub)
+-- app (2, 3, sentinel)
+-- app (loop)
+-- app (0, 10, 2, sentinel)
+-- app (sentinel, 7, 8, sentinel, sentinel)
