2ec1aab5e5d2b6e5eb3b67bfad986c484e4da261
Alisa Dolinsky <alisa.dolinsky.1984@gmail.com>
2015-09-06 12:00:45 +0200

    Proper quoting.

--- src/gluttony.lua
+++ src/gluttony.lua
@@ -1,19 +1,26 @@
 local sentinel = {}
+local end_program = {}
 
-local function exit (...) print ('result', ...) print ('exit') end
+local function exit (operands) print ('result', unpack (operands)) print ('exit') end
+local function quote (operands) return { operands } end
 
 local function operator_prototype (operators, execute)
+    local delimiter = sentinel
+    if execute == exit then delimiter = end_program end
     local operands = {}
+    local handle_operands = function (_) table.insert (operands, _) end
+    local handle_operators = function (_) table.insert (operators, operator_prototype (operators, _)) end
+    if execute == quote then handle_operators = handle_operands end  -- suspend execution of quoted
     return function (...)
         local args = {...}
         local _
         while (function () _ =
             table.remove (args, 1)
-            return not (_ == nil or _ == sentinel) end) ()
+            return not (_ == nil or _ == delimiter) end) ()
         do
-            table.insert (operands, _)
+            if type (_) == 'function' then handle_operators (_) return else handle_operands (_) end
         end
-        if _ == sentinel then
+        if _ == delimiter then
             table.remove (operators)
             local output = { execute (operands, operators) }
             for i, v in ipairs (args) do table.insert (output, v) end
@@ -24,16 +31,14 @@ end
 
 function eval (operands, operators)
     for i, v in ipairs (operands) do
-        if type (v) == 'function' then table.insert (operators, operator_prototype (operators, v)) else
             local _ = operators[#operators] (v)
-            if operators[#operators] == exit then return operators[#operators] (unpack (_), select (i + 1, unpack (operands))) end
-            while _ ~= nil do _ = operators[#operators] (unpack (_)) end
-        end
+            while not (_ == nil or #operators == 0) do _ = operators[#operators] (unpack (_)) end
     end
 end
 
 function processor_prototype ()
-    local operators = { exit }
+    local operators = {}
+    table.insert (operators, operator_prototype (operators, exit))
     return function (...) eval ({...}, operators) end
 end
 
@@ -58,7 +63,7 @@ loop = function (operands, operators)
 end
 
 local app = processor_prototype ()
-app (add, 1, 2, 3, 4, 5, 6, sub, 2, 3, sentinel, loop, 0, 10, 2, sentinel, sentinel, 7, 8, sentinel, sentinel)
+app (add, 1, 2, 3, 4, 5, 6, sub, 2, 3, sentinel, loop, 0, 10, 2, sentinel, sentinel, 7, 8, sentinel, sentinel, quote, add, 1, 2, 3, sentinel, end_program)
 -- app (1, 2)
 -- app (3, 4)
 -- app (5, 6)
