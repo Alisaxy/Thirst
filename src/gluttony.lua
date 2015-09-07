99c8b21386c2e94e57dfbcc6cc43ed785ba660c7
Alisa Dolinsky <alisa.dolinsky.1984@gmail.com>
2015-09-07 19:46:27 +0200

    Refactored, added a polyfill to fix different Lua versions and implemented a fork primitive.

--- src/gluttony.lua
+++ src/gluttony.lua
@@ -1,65 +1,73 @@
-local sentinel = {}
-local end_program = {}
+table.unpack = unpack  -- polyfill
 
-local function exit (operands) print ('result', unpack (operands)) print ('exit') end
+local sentinel = {}
+local quote_sentinel = {}
+
+local function exit (operands) print ('result', table.unpack (operands)) print ('exit') end
 local function quote (operands) return operands end
 
-local function operator_prototype (operators, execute)
-    local delimit_counter = 1
-    local delimiter = sentinel
-    local operands = {}
-    
-    local handle_operands = function (_) table.insert (operands, _) end
-    
-    local handle_quoted_operators = function (_)
-        delimit_counter = delimit_counter + 1
-        handle_operands (_)
-    end
-    local handle_operators = function (_) table.insert (operators, operator_prototype (operators, _)) end
-    
-    local check_sequence = function (_) return not (_ == nil or _ == delimiter) end
-    local check_sequence_quoted = function (_) return _ ~= nil and delimit_counter > 0 end
-    
-    if execute == quote then -- suspend execution of quoted
-        check_sequence = check_sequence_quoted
-        handle_operators = handle_quoted_operators
-    elseif  -- a special end_program delimiter is required to exit the program
-        execute == exit then delimiter = end_program
-    end  
-    
-    return function (...)
+local operator_prototype_base
+local operator_prototype
+
+operator_prototype_base = function (operators, execute)
+    local base
+    base = {
+    delimiter = sentinel,
+    delimit_counter = 1,
+    operands = {},
+    handle_operators = function (_) table.insert (operators, operator_prototype (operators, _)) end,
+    handle_operands = function (_) table.insert (base.operands, _) end,
+    run = function (...)
         local args = {...}
         local _
         while (function () _ =
             table.remove (args, 1)
-            if _ == delimiter then delimit_counter = delimit_counter - 1 end
-            return check_sequence (_) end) ()
+            if _ == base.delimiter then base.delimit_counter = base.delimit_counter - 1 end
+            return _ ~= nil and base.delimit_counter > 0 end) ()
         do
-            if type (_) == 'function' then handle_operators (_) else handle_operands (_) end
+            if type (_) == 'function' then base.handle_operators (_) else base.handle_operands (_) end
         end
         -- debug
-        -- print (delimit_counter)
+        print (base.delimit_counter)
         -- debug
-        if delimit_counter <= 0 then
-            table.remove (operators)
-            local output = { execute (operands, operators) }
+        if base.delimit_counter <= 0 then
+            for i = #operators, 1, -1 do  -- remove self
+                if operators[i] == base.run then table.remove (operators, i) break end
+            end
+            local output = { execute (base.operands, operators) }
             for i, v in ipairs (args) do table.insert (output, v) end
             return output
         end
-    end
+    end }
+    return base
+end
+
+operator_prototype = function (operators, execute)
+    local base = operator_prototype_base (operators, execute)
+    -- polymorphic dispatch
+    if execute == quote then -- suspend execution of quoted
+        base.delimiter = quote_sentinel
+        base.handle_operators = function (_)
+            if _ == quote_sentinel then base.delimit_counter = base.delimit_counter + 1 else base.handle_operands (_) end
+        end
+    elseif execute == exit then base.delimiter = exit end
+    return base.run
 end
 
 function eval (operands, operators)
     for i, v in ipairs (operands) do
             local _ = operators[#operators] (v)
-            while not (_ == nil or #operators == 0) do _ = operators[#operators] (unpack (_)) end
+            while not (_ == nil or #operators == 0) do _ = operators[#operators] (table.unpack (_)) end
     end
 end
 
 function processor_prototype ()
     local operators = {}
     table.insert (operators, operator_prototype (operators, exit))
-    return function (...) eval ({...}, operators) end
+    return function (...) if #operators > 0 then eval ({...}, operators)
+        else
+            print ('the program had ended its execution, any further invocations are futile') end
+    end
 end
 
 function eval_quotes (operands, operators)
@@ -69,7 +77,7 @@ end
 local function combinator_prototype (op)
     return function (operands)
         local _ = table.remove (operands, 1)
-        for i, v in ipairs (operands) do if type(v) == 'table' then print ('table', unpack (v)) end _ = op (_, v) end
+        for i, v in ipairs (operands) do if type(v) == 'table' then print ('table', table.unpack (v)) end _ = op (_, v) end
         return _
     end
 end
@@ -84,9 +92,22 @@ loop = function (operands, operators)
     local idx = operands[1]
     local limit = operands[2]
     local step = operands[3]
-    print('loop', unpack (operands))
+    print('loop', table.unpack (operands))
     if idx < limit then table.insert (operators, operator_prototype (operators, loop)) return idx + step, limit, step, sentinel end
 end
 
+local fork = function (operands, operators)
+    table.insert (operators, operator_prototype (operators, eval_quotes))
+    local left = operands[1]
+    local right = operands[2]
+    local predicate = operands[3]
+    local evaluated
+    if predicate then evaluated = left else evaluated = right end
+    return evaluated, sentinel
+end
+
 local app = processor_prototype ()
-app (add, eval_quotes, quote, div, 1, 2, 3, add, 4, 5, 6, sentinel, sentinel, sentinel, quote, div, 1, 2, 3, add, 4, 5, 6, sentinel, sentinel, sentinel, sentinel, sentinel, end_program)
+app (add, eval_quotes, quote, div, 1, 2, 3, add, 4, 5, 6, sentinel, sentinel, quote_sentinel, quote, div, 1, 2, 3, add, 4, 5, 6, sentinel, sentinel, quote_sentinel, sentinel, sentinel, exit)
+app ()
+app2 = processor_prototype ()
+app2 (fork, quote, add, 1, 2, 3, sentinel, quote_sentinel, quote, sub, 1, 2, 3, sentinel, quote_sentinel, false, sentinel, exit)
