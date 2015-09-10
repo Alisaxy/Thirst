b1fc0c470568bf4ac089cb6bed9e30acbe805d72
Alisa Dolinsky <alisa.dolinsky.1984@gmail.com>
2015-09-10 20:59:13 +0200

    Some recursion, global defines. Added lots of todo's.

--- README.md
+++ README.md
@@ -10,13 +10,21 @@ As of this moment the interpreter is being prototyped in Lua. It is planned to b
 * quotes
 * quote evaluator
 * conditional fork
+* some tail recursion, needs testing
 
 ##Todo:
-* proper recursion
+* rest of the core
+* proper recursion from the language itself
 * lists
 * hash-maps
 * string evaluator
+* implement a pool hash-map per application and access restrictions on functions a la persistent databases only in memory to do away with the traditional scoping rules
+* make powerful profiling and debugging tools
 * port to a proper systems language such as Rust
 * make embeddable
 * implement a bytecode VM
+* test everything thoroughly
 * write the all encompassing language reference and an extensive documentation
+* add safe concurrency (functional, slot-based)
+* think on async programming, which should be interesting because one of Thirst' emerging properties could be just that
+* implement optional incremental evaluation for functions that don't care about async
--- src/thirst.lua
+++ src/thirst.lua
@@ -1,7 +1,8 @@
-table.unpack = unpack  -- polyfill
+if not table.unpack and unpack then table.unpack = unpack end  -- polyfill
 
 local sentinel = {}
 local quote_sentinel = {}
+local void = {}
 
 local function exit (operands) print ('result', table.unpack (operands)) print ('exit') end
 local function quote (operands) return operands end
@@ -9,13 +10,13 @@ local function quote (operands) return operands end
 local operator_prototype_base
 local operator_prototype
 
-operator_prototype_base = function (operators, execute)
+operator_prototype_base = function (operators, execute, definitions)
     local base
     base = {
     delimiter = sentinel,
     delimit_counter = 1,
     operands = {},
-    handle_operators = function (_) table.insert (operators, operator_prototype (operators, _)) end,
+    handle_operators = function (_) table.insert (operators, operator_prototype (operators, _, definitions)) end,
     handle_operands = function (_) table.insert (base.operands, _) end,
     run = function (...)
         local args = {...}
@@ -23,18 +24,18 @@ operator_prototype_base = function (operators, execute)
         while (function () _ =
             table.remove (args, 1)
             if _ == base.delimiter then base.delimit_counter = base.delimit_counter - 1 end
-            return _ ~= nil and base.delimit_counter > 0 end) ()
+            return not (_ == nil or _ == void) and base.delimit_counter > 0 end) ()
         do
             if type (_) == 'function' then base.handle_operators (_) else base.handle_operands (_) end
         end
         -- debug
-        print (base.delimit_counter)
+        -- print (base.delimit_counter)
         -- debug
         if base.delimit_counter <= 0 then
             for i = #operators, 1, -1 do  -- remove self
                 if operators[i] == base.run then table.remove (operators, i) break end
             end
-            local output = { execute (base.operands, operators) }
+            local output = { execute (base.operands, operators, definitions) }
             for i, v in ipairs (args) do table.insert (output, v) end
             return output
         end
@@ -42,8 +43,8 @@ operator_prototype_base = function (operators, execute)
     return base
 end
 
-operator_prototype = function (operators, execute)
-    local base = operator_prototype_base (operators, execute)
+operator_prototype = function (operators, execute, definitions)
+    local base = operator_prototype_base (operators, execute, definitions)
     -- polymorphic dispatch
     if execute == quote then -- suspend execution of quoted
         base.delimiter = quote_sentinel
@@ -54,17 +55,25 @@ operator_prototype = function (operators, execute)
     return base.run
 end
 
-function eval (operands, operators)
+function eval (operands, operators, definitions)
     for i, v in ipairs (operands) do
             local _ = operators[#operators] (v)
-            while not (_ == nil or #operators == 0) do _ = operators[#operators] (table.unpack (_)) end
+            while not (_ == nil or #operators == 0) do  -- check!
+              local args = {}
+              for i, v in ipairs (_) do  -- TODO: handle more than one returned function!
+                if type (v) == 'function' then table.insert (operators, operator_prototype (operators, v, definitions))
+                else table.insert (args, v) end
+              end
+              _ = operators[#operators] (table.unpack (args))
+            end
     end
 end
 
 function processor_prototype ()
+    local definitions = {}
     local operators = {}
-    table.insert (operators, operator_prototype (operators, exit))
-    return function (...) if #operators > 0 then eval ({...}, operators)
+    table.insert (operators, operator_prototype (operators, exit, definitions))
+    return function (...) if #operators > 0 then eval ({...}, operators, definitions)
         else
             print ('the program had ended its execution, any further invocations are futile') end
     end
@@ -77,7 +86,11 @@ end
 local function combinator_prototype (op)
     return function (operands)
         local _ = table.remove (operands, 1)
-        for i, v in ipairs (operands) do if type(v) == 'table' then print ('table', table.unpack (v)) end _ = op (_, v) end
+        for i, v in ipairs (operands) do
+            -- debug
+            -- if type(v) == 'table' then print ('table', table.unpack (v)) end
+            -- debug
+            _ = op (_, v) end
         return _
     end
 end
@@ -93,17 +106,31 @@ loop = function (operands, operators)
     local limit = operands[2]
     local step = operands[3]
     print('loop', table.unpack (operands))
-    if idx < limit then table.insert (operators, operator_prototype (operators, loop)) return idx + step, limit, step, sentinel end
+    if idx < limit then return loop, idx + step, limit, step, sentinel else return void end
 end
 
+-- [ fork [  ] void true . ]
+
 local fork = function (operands, operators)
-    table.insert (operators, operator_prototype (operators, eval_quotes))
+    -- table.insert (operators, operator_prototype (operators, eval_quotes))
     local left = operands[1]
     local right = operands[2]
     local predicate = operands[3]
     local evaluated
     if predicate then evaluated = left else evaluated = right end
-    return evaluated, sentinel  -- evaluate right away
+    return eval_quotes, evaluated, sentinel  -- evaluate right away
+end
+
+local define = function (operands, operators, definitions)
+    local symbol = operands[1]
+    local quotation = operands[2]
+    definitions[symbol] = quotation
+    return void
+end
+
+local refer = function (operands, operators, definitions)
+    local symbol = operands[1]
+    return definitions[symbol]
 end
 
 local app = processor_prototype ()
@@ -111,3 +138,7 @@ app (add, eval_quotes, quote, div, 1, 2, 3, add, 4, 5, 6, sentinel, sentinel, qu
 app ()
 app2 = processor_prototype ()
 app2 (fork, quote, add, 1, 2, 3, sentinel, quote_sentinel, quote, sub, 1, 2, 3, sentinel, quote_sentinel, false, sentinel, exit)
+app3 = processor_prototype ()
+app3 (loop, 0, 10, 2, sentinel, exit)
+app4 = processor_prototype ()
+app4 (define, 'test', quote, 1, 2, 3, quote_sentinel, sentinel, eval_quotes, refer, 'test', sentinel, sentinel, exit)
